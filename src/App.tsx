f78423d73c8871f7378356ddade381620c9fe07a
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2022-11-01 13:23:44 -0600

    adds state for board + passes static test

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,31 @@ import React, { useState } from 'react'
 import './App.css'
 
 export function App() {
+  const [game, setGame] = useState({
+    board: [
+      [' ', ' ', ' '],
+      [' ', ' ', ' '],
+      [' ', ' ', ' '],
+    ],
+    id: null,
+    winner: null,
+  })
+
   return (
     <div>
       <h1>
         React-Tac-Toe: <button>New</button>
       </h1>
       <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li>{game.board[0][0]}</li>
+        <li>{game.board[0][1]}</li>
+        <li>{game.board[0][2]}</li>
+        <li>{game.board[1][0]}</li>
+        <li>{game.board[1][1]}</li>
+        <li>{game.board[1][2]}</li>
+        <li>{game.board[2][0]}</li>
+        <li>{game.board[2][1]}</li>
+        <li>{game.board[2][2]}</li>
       </ul>
     </div>
   )
