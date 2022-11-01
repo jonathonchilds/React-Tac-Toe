277bbb507819990ee1d38168eabe6b9aa69e8d19
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2022-11-01 13:03:37 -0600

    initial commit with starter HTML & CSS (passes static test, too)

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import './App.css'
+
+export function App() {
+  return (
+    <div>
+      <h1>
+        React-Tac-Toe: <button>New</button>
+      </h1>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  )
+}
