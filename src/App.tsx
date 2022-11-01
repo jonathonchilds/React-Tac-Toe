198d43dac286e9285af6376296a49dec2c2f9869
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2022-11-01 13:41:53 -0600

    connects actions for clicking on each cell - verified by console.log in developer console

--- src/App.tsx
+++ src/App.tsx
@@ -12,21 +12,25 @@ export function App() {
     winner: null,
   })
 
+  function handleClickCell(row: number, column: number) {
+    console.log(`You clicked on row ${row} and column ${column}`)
+  }
+
   return (
     <div>
       <h1>
         React-Tac-Toe: <button>New</button>
       </h1>
       <ul>
-        <li>{game.board[0][0]}</li>
-        <li>{game.board[0][1]}</li>
-        <li>{game.board[0][2]}</li>
-        <li>{game.board[1][0]}</li>
-        <li>{game.board[1][1]}</li>
-        <li>{game.board[1][2]}</li>
-        <li>{game.board[2][0]}</li>
-        <li>{game.board[2][1]}</li>
-        <li>{game.board[2][2]}</li>
+        <li onClick={() => handleClickCell(0, 0)}>{game.board[0][0]}</li>
+        <li onClick={() => handleClickCell(0, 1)}>{game.board[0][1]}</li>
+        <li onClick={() => handleClickCell(0, 2)}>{game.board[0][2]}</li>
+        <li onClick={() => handleClickCell(1, 0)}>{game.board[1][0]}</li>
+        <li onClick={() => handleClickCell(1, 1)}>{game.board[1][1]}</li>
+        <li onClick={() => handleClickCell(1, 2)}>{game.board[1][2]}</li>
+        <li onClick={() => handleClickCell(2, 0)}>{game.board[2][0]}</li>
+        <li onClick={() => handleClickCell(2, 1)}>{game.board[2][1]}</li>
+        <li onClick={() => handleClickCell(2, 2)}>{game.board[2][2]}</li>
       </ul>
     </div>
   )
