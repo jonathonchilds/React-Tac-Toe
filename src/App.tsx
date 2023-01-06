7b89bb53ce9f6b32b1a1e43ecf9c1baba35bc42d
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2023-01-05 23:11:55 -0700

    working <3

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import './App.css'
-import axios from 'axios'
 
 export function App() {
   const [game, setGame] = useState({
@@ -12,48 +11,30 @@ export function App() {
     id: null,
     winner: null,
   })
-
-  function handleNewGame() {
-    const handleNewGame = async () => {
-      const response = await axios.post('summer-dust-2476.fly.dev/games')
-      if (response) {
-        console.log(response)
-        const newGame = response.data
-
-        setGame(newGame)
-        console.log(newGame)
-      }
-    }
-  }
-
-  async function handleClickCell(row: number, column: number) {
-    const url = `summer-dust-2476.fly.dev/game/${game.id}`
-    try {
-      const response = await axios.post(url, { row: row, column: column })
-      const newGame = await response.data
+  async function handleNewGame() {
+    const response = await fetch('https://tic-tac-toe-api.fly.dev/game', {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+    })
+    if (response.ok) {
+      const newGame = await response.json()
       setGame(newGame)
-    } catch (err) {
-      console.log(err)
     }
   }
-
-  // async function handleClickCell(row: number, column: number) {
-  //   const url = `summer-dust-2476.fly.dev/game/${game.id}`
-  //   const body = { row: row, column: column }
-  //   const response = await fetch(url, {
-  //     method: 'POST',
-  //     headers: { 'content-type': 'application/json' },
-  //     body: JSON.stringify(body),
-  //   })
-  //   console.log(response)
-  //   if (response.ok) {
-  //     const newGame = await response.json()
-  //     setGame(newGame)
-  //   }
-  // }
-
+  async function handleClickCell(row: number, column: number) {
+    const url = `https://tic-tac-toe-api.fly.dev/game/${game.id}`
+    const body = { row: row, column: column }
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(body),
+    })
+    if (response.ok) {
+      const newGame = await response.json()
+      setGame(newGame)
+    }
+  }
   const header = game.winner ? `${game.winner} is the winner!` : 'React Tac Toe'
-
   return (
     <div>
       <h1>
